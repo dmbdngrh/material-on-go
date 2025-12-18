a3c5a5fb203aa3255d909aa724bbac12273f74b1
Danielsitanggang05 <arifinsitanggang17@gmail.com>
2025-12-18 22:53:20 +0700

    Finalize UI/UX Material-on-Go (navbar, home, profile)

--- app.js
+++ app.js
@@ -1,11 +1,17 @@
 const express = require('express')
+const expressLayouts = require('express-ejs-layouts')
+
 const app = express()
 const port = 3000
 
 app.set('view engine', 'ejs')
 app.use(express.urlencoded({ extended: false }))
 
-// ROUTES UI
+// aktifkan layout
+app.use(expressLayouts)
+app.set('layout', 'main')
+
+// ROUTEs UI
 app.get('/', (req, res) => {
   res.render('landing')
 })
@@ -18,16 +24,45 @@ app.get('/login', (req, res) => {
   res.render('login')
 })
 
+app.post('/login', (req, res) => {
+  res.redirect('/home')
+})
+
 app.get('/onboarding', (req, res) => {
   res.render('onboarding')
 })
 
 app.get('/home', (req, res) => {
-  res.render('home')
+  const stores = [
+    {
+      name: 'Toko Bangunan Jaya',
+      location: 'Jakarta Selatan',
+      image: 'https://images.unsplash.com/photo-1604328698692-f76ea9498e76'
+    },
+    {
+      name: 'Material Sejahtera',
+      location: 'Bandung',
+      image: 'https://images.unsplash.com/photo-1586864387967-d02ef85d93e8'
+    },
+    {
+      name: 'Sumber Bangunan',
+      location: 'Surabaya',
+      image: 'https://images.unsplash.com/photo-1600566753086-00f18fb6b3ea'
+    }
+  ]
+
+  res.render('home', { stores })
 })
 
 app.get('/profile', (req, res) => {
-  res.render('profile')
+  const user = {
+    name: 'Daniel Alonso',
+    email: 'daniel@mail.com',
+    role: 'Buyer',
+    photo: 'https://randomuser.me/api/portraits/men/32.jpg'
+  }
+
+  res.render('profile', { user })
 })
 
 app.listen(port, () => {
